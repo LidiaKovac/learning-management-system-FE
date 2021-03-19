7c6b0af81b85555c1c3d3e372c1492e08f11ea82
LidiaKovac <lidia.kovac1998@gmail.com>
2021-03-19 19:05:24 +0100

    adds assets for menu and adds teacher dash to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 import StudentDashboard from './pages/Dashboard/StudentDashboard';
 import Redirect from "./pages/Redirect/Redirect"
-import { useSelector } from 'react-redux';
+import TeacherDashboard from './pages/Dashboard/TeacherDashboard';
+import Notes from './pages/Notes/Notes';
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+     <Route exact path="/redirect"><Redirect/></Route>
+
      <Route exact path="/"><Login/></Route> 
      <Route exact path="/join"><SignUp/></Route>
-     <Route exact path="/redirect"><Redirect/></Route>
+     
      <Route exact path="/studentdash"><StudentDashboard/></Route>
+     <Route exact path="/teacherdash"><TeacherDashboard/></Route>
+
+     <Route exact path="/notes"><Notes/></Route>
     </BrowserRouter>
   );
 }
