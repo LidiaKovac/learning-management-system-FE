9554c463e3c81bddd43544545920442ff4d9023f
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-03-30 10:39:29 +0200

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,9 @@
 export const get_token_from_cookies = (cookies:String) => {
+    console.log(document.cookie)
+    console.log(cookies)
     const arr = cookies?.split(";") // splits in an array of string, each containing a cookie
+    console.log(arr)
     const token = arr[1]?.split("=") 
+    console.log(token)
     return token && token[1]
 }
